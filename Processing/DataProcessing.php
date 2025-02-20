b1b3f2df07ed9fdb381de504313dfe164627e9fa
degrandelucas <lucasdegrande15@gmail.com>
2025-02-20 19:26:56 -0300

    [bug] array saindo vazio

--- Processing/DataProcessing.php
+++ Processing/DataProcessing.php
@@ -1,2 +1,11 @@
 <?php
 
+$user = [
+    'nome' => $_POST['name'],
+    'email' => $_POST['email'],
+    'telefone' => $_POST['cellphone'],
+    'instrumento' => $_POST['instrument']
+];
+
+var_dump($_POST);
+var_dump($user);
